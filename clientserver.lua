13febcc816ded8b47976a2dcb2011a108fcd43d1
romanticjo <romanticjo88@gmail.com>
2015-08-30 22:52:33 -0700

    refactored to run as an api

--- clientserver.lua
+++ clientserver.lua
@@ -1,31 +1,8 @@
-print( 'Client/Server - Sample' )
-print( 'We are computer: ' .. os.getComputerID())
 
-local args = { ... }
-local role = args[1]
-local protocol = args[2]
-local modemLocation = args[3]
-assert(role == 'client' or role == 'server', 'Specify "client" or "server"')
-assert(protocol, 'Specify a server')
-assert(modemLocation == 'left' or modemLocation == 'right' 
-	or modemLocation == 'top' or modemLocation == 'bottom'
-	or modemLocation == 'front' or modemLocation == 'back', 'Specify side for the rednet conenction.');
-
-rednet.open(modemLocation)
-
+local protocol
 local name
-if role == 'server' then
-	local hosts = {rednet.lookup(protocol, 'server')}
-	local thisAsHost = hosts[1] == os.computerID()
-	assert(thisAsHost or 0 == #hosts, 'Another server exists.')
-	name = 'server'
-else
-	name = 'client-'..os.computerID()
-end
-rednet.host(protocol, name)
-print('Registered '..name..' on protocol '..protocol)
 
-function parse (str) 
+local function parse (str) 
 	local f, args = nil, {}
 	for token in string.gmatch(str, '[^|]+') do
 		if not f then f = token else
@@ -35,16 +12,14 @@ function parse (str)
 	return f, args
 end
 
-function server_up (sid)
-end
 
 local clients = {}
-function client_up (sid, name)
+local function client_up (sid, name)
 	-- a new client comes to the network.
 	rednet.send(sid, 'server_id|'..os.computerID(), protocol)
 end
 
-function connect(sid, name)
+local function connect(sid, name)
 	table.insert(clients, sid)
 	print('Registered client:' .. sid .. ' name:' .. name)
 	print('Total clients: ' .. #clients)
@@ -53,19 +28,19 @@ end
 
 local server
 -- server authoritative answer
-function server_up (sid)
+local function server_up (sid)
 	print('Server announcing, connecting to: ' .. sid)
 	rednet.send(sid, 'connect|' .. name, protocol)
 end
 -- new client, inform of our server id
-function peer_up (sid)
+local function peer_up (sid)
 	if server then
 		print('Peer:'..sid..' is announcing.  Replying with server id:'..server)
 		rednet.send(sid, 'server_id|' .. server, protocol)
 	end
 end
 -- non-authoritatve answer, connect explicitely.
-function server_id (sid, serverId)
+local function server_id (sid, serverId)
 	if not server then
 		print( 'Peer:' .. sid .. ' points to Server:' .. serverId);
 		if tonumber(serverId) ~= os.getComputerID() then 
@@ -77,29 +52,32 @@ function server_id (sid, serverId)
 end
 
 -- server connect confirmation
-function connect_done (sid)
-	if sid ~= server then 
-		server = sid
-		print('Connected to server: ' .. sid)
-	end 
+local function connect_done (sid)
+	server = sid
+	print('Connected to server: ' .. sid)
 end
 
-clientCommands = {
+local clientCommands = {
 	server_up = server_up,
 	client_up = peer_up,
 	server_id = server_id,
 	connect_done = connect_done
 }
 
-serverCommands = {
+local serverCommands = {
 	client_up = client_up,
 	connect = connect
 }
 
-function run (commandTable)
+local function loop (commandTable, timerFunc, delay)
 	assert(commandTable)
 	while true do
-		local sid, msg, p = rednet.receive(protocol)
+		local sid, msg, p;
+		if timerFunc then 
+			sid, msg, p = rednet.receive(protocol, timerFunc, delay or 30) 
+		else
+			sid, msg, p = rednet.receive(protocol);
+		end
 		assert(p == protocol)
 		print('network: ' .. msg)
 		cmd, args = parse(msg)
@@ -110,15 +88,43 @@ function run (commandTable)
 	end
 end
 
-if role == 'server' then
-	-- collect existing clients
-	print('Annoucing server on ' .. protocol)
-	rednet.broadcast('server_up', protocol)
-	print('Running...')
-	run(serverCommands)
-else
-	print('Announcing client on ' .. protocol)
-	rednet.broadcast('client_up|'..name, protocol)
-	print('Running...')
-	run(clientCommands)
+function run (role, _protocol, modemLocation, timerFunc, delay) 
+	print( 'Client/Server - Sample' )
+	print( 'We are computer: ' .. os.getComputerID())
+
+	protocol = _protocol
+
+	assert(role == 'client' or role == 'server', 'Specify "client" or "server"')
+	assert(protocol, 'Specify a server')
+	assert(modemLocation == 'left' or modemLocation == 'right' 
+		or modemLocation == 'top' or modemLocation == 'bottom'
+		or modemLocation == 'front' or modemLocation == 'back', 'Specify side for the rednet conenction.');
+
+	rednet.open(modemLocation)
+
+	if role == 'server' then
+		local hosts = {rednet.lookup(protocol, 'server')}
+		local thisAsHost = hosts[1] == os.computerID()
+		assert(thisAsHost or 0 == #hosts, 'Another server exists.')
+		name = 'server'
+	else
+		name = 'client-'..os.computerID()
+	end
+	rednet.host(protocol, name)
+	print('Registered '..name..' on protocol '..protocol)
+
+	if role == 'server' then
+		-- collect existing clients
+		print('Annoucing server on ' .. protocol)
+		rednet.broadcast('server_up', protocol)
+		print('Running...')
+		loop(serverCommands, timerFunc, delay)
+	else
+		print('Announcing client on ' .. protocol)
+		rednet.broadcast('client_up|'..name, protocol)
+		print('Running...')
+		loop(clientCommands, timerFunc, delay)
+	end
 end
+
+
