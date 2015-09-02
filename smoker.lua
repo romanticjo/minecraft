3295845dd93af7d7e6d9b02130fb6d5b333029e8
romanticjo <romanticjo88@gmail.com>
2015-09-01 23:26:32 -0700

    Control the smoke stack for a kiln

---
+++ smoker.lua
@@ -0,0 +1,35 @@
+print('Launching Furnace Monitoring')
+
+os.loadAPI('clienterserver.lua')
+local server=(getfenv())["clienterserver.lua"]
+local furnaces
+
+function getConnectedFurnaces () 
+	local p = peripheral.getNames()
+	local furnaces = {}
+	for i = 1, #p do
+		s, e = string.find(p[i], 'container_furnace' );
+		if s ~= nil and s == 1 then
+			furnaces[ p[i] ] = peripheral.wrap( p[i] )
+		end
+	end
+	return furnaces
+end
+
+function setSmoker (burning)
+	redstone.setOutput('right', not burning)
+end
+
+function updateSmoker ()
+	-- set smoker state
+	local isBurning = false
+	for n, p in pairs(furnaces) do
+		isBurning = p.isBurning() or isBurning
+		--print( 'Found: ' .. n .. ', burning: ' ..  (p.isBurning() and 'true' or 'false')  )
+	end
+	setSmoker( isBurning )
+	server.send('smoking|' .. tostring(isBurning))
+end
+
+furnaces = getConnectedFurnaces()
+server.run(client, 'jo-factory', 'back', updateSmoker, 2)
